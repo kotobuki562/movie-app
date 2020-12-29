26db496fd17429d931efddc5a1d170d2cbc10f23
MaQueen <shusineko@icloud.com>
2020-12-29 14:56:46 +0900

    APIkeyCreate

--- src/context.js
+++ src/context.js
@@ -1,14 +1,16 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect } from "react";
 // make sure to use https
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
-const AppContext = React.createContext()
+// `http://www.omdbapi.com/?i=tt3896198&apikey=8fc3a36c`;
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+console.log(API_ENDPOINT);
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };

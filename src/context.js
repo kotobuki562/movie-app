425792d6e42af460d3ff2ed1e643eb6a3e32ada3
MaQueen <shusineko@icloud.com>
2020-12-29 18:23:00 +0900

    Loading&Error&Movie.useState

--- src/context.js
+++ src/context.js
@@ -2,10 +2,12 @@ import React, { useState, useContext, useEffect } from "react";
 // make sure to use https
 // `http://www.omdbapi.com/?i=tt3896198&apikey=8fc3a36c`;
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
-console.log(API_ENDPOINT);
+
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: "" });
   return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
 };
 // make sure use

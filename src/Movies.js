23955dd74d9f0e58a2a02eeb51a235495c147b85
MaQueen <shusineko@icloud.com>
2020-12-31 11:08:03 +0900

    searchMovieForm/create

--- src/Movies.js
+++ src/Movies.js
@@ -13,7 +13,6 @@ const Movies = () => {
     <section className="movies">
       {movies.map((movie) => {
         const { imdbID: id, Poster: poster, Title: title, Year: year } = movie;
-        console.log(poster);
         return (
           <Link to={`/movies/${id}`} key={id} className="movie">
             <article>
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,7 +1,18 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 const SearchForm = () => {
-  return <h2>search component</h2>
-}
+  const { query, setQuery, error } = useGlobalContext();
+  return (
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+      <h2>serch movies</h2>
+      <input
+        type="text"
+        className="form-input"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+};
 
-export default SearchForm
+export default SearchForm;

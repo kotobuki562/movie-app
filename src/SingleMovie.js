c8497e44f22d5a96895d5c8830b9b664a5c2d0c1
MaQueen <shusineko@icloud.com>
2020-12-31 15:08:33 +0900

    SingleMovie.SetError

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -5,18 +5,38 @@ import { API_ENDPOINT } from "./context";
 const SingleMovie = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setLoading] = useState(true);
   const [error, setError] = useState({ show: false, msg: "" });
 
   const fetchMovie = async (url) => {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    if (data.Response === "False") {
+      setError({ show: true, msg: data.Error });
+      setLoading(false);
+    } else {
+      setMovie(data);
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
-    fetch();
+    fetchMovie(`${API_ENDPOINT}&i=${id}`);
   }, [id]);
+
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+  if (error.show) {
+    return (
+      <div className="page-error">
+        <h1>{error.msg}</h1>
+        <Link to="/" className="btn">
+          back to movies
+        </Link>
+      </div>
+    );
+  }
 
   return <h2>single movie</h2>;
 };

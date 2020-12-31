a5c228ba0750e2375e31d39ae071c4b1f9c33f5c
MaQueen <shusineko@icloud.com>
2020-12-31 17:06:48 +0900

    commit

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
-import { API_ENDPOINT } from "./context";
 import useFetch from "./useFetch";
 
 const SingleMovie = () => {
